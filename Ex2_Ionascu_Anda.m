934d614bd8f81a8841cc8df44747ea6f8456cfc3
AndaIonascu <andageo96@yahoo.com>
2016-11-06 14:29:15 +0200

    Add files via upload

---
+++ Ex2_Ionascu_Anda.m
@@ -0,0 +1,8 @@
+A=randn(1,10) %am generat vectorul v, cu elemente aleatoare, dupa o distributie gaussiana
+j=0 %am initializat un contor j pentru a numara cate elemente din v sunt negative
+for i=1:1:10 %parcurgem vectorul
+    if A(i)<0 
+        j=j+1 %de fiecare data cand intalnim un numar negativ, j-ul se incrementeaza
+          end
+    
+end
